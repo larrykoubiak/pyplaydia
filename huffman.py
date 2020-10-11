542b832ddc1d1ce7fa81338adbf53442735a3b2d
Larry Koubiak <larrykoubiak@gmail.com>
2020-10-11 21:20:39 +0200

    finished huffman encode/decode functions

--- huffman.py
+++ huffman.py
@@ -6,6 +6,7 @@ class BitBuffer:
         self.__values = bytearray()
         self.__buffer = 0
         self.__pos = 0
+        self.__index = 0
 
     def push(self, bit):
         self.__buffer <<= 1
@@ -13,16 +14,42 @@ class BitBuffer:
         self.__pos += 1
         if self.__pos == 8:
             self.__values.append(self.__buffer)
+            if self.__buffer == 0xFF:
+                self.__values.append(0x00)
             self.__buffer = 0
             self.__pos = 0
+    
+    def pop(self):
+        self.__buffer = (self.__values[self.__index] >> (7 - (self.__pos)))
+        self.__pos += 1
+        if self.__pos > 7:
+            self.__pos = 0
+            self.__index += 1
+            if self.__index < len(self.__values):
+                if self.__values[self.__index] == 0xFF:
+                    self.__index += 1
+                    if self.__values[self.__index] == 0x00:
+                        self.__index += 1
+                    elif self.__values[self.__index] == 0xD9:
+                        return None
+                    else:
+                        print("error")
+            else:
+                return None
+        return self.__buffer & 0x01
     
     @property
     def Values(self):
         values = self.__values
         if self.__pos > 0:
             buffer = self.__buffer << (8 - self.__pos)
+            # buffer |= (0xff >> self.__pos)
             values.append(buffer)
         return values
+
+    @Values.setter
+    def Values(self, value):
+        self.__values = value
 
 class HuffmanNode:
     def __init__(self, val, freq):
@@ -48,20 +75,21 @@ class HuffmanNode:
         return "'%s': %d" % (self.value, self.freq)
 
 class Huffman:
-    def __init__(self, input_string = None):
+    def __init__(self):
         self.root = None
         self.codes = {}
         self.reverse_codes = {}
-        if input_string is not None:
-            self.__buildtree(input_string)
     
-    def __buildtree(self, input_string):
+    def FromString(self, input_string):
         frequencies = {}
         heap = []
-        for c in input_string:
+        input = bytearray()
+        input.extend(map(ord, input_string))
+        for c in input:
             if c not in frequencies:
                 frequencies[c] = 0
             frequencies[c] += 1
+        frequencies[0xFF] = 0
         for k, v in frequencies.items():
             node = HuffmanNode(k, v)
             heappush(heap, node)
@@ -85,16 +113,35 @@ class Huffman:
             return
         self.__traversetree(node.left, code + "0")
         self.__traversetree(node.right, code + "1")
+            
 
     def Encode(self, string):
         buffer = BitBuffer()
-        for c in string:
+        input = bytearray()
+        input.extend(map(ord, string))
+        input.append(0xFF)
+        for c in input:
             code = self.codes[c]
             for d in code:
                 buffer.push(int(d))
         return buffer.Values
-
-
+    
+    def Decode(self, values):
+        buffer = BitBuffer()
+        buffer.Values = values
+        b = buffer.pop()
+        node = self.root
+        decvals = bytearray()
+        while b is not None:
+            node = node.left if b == 0 else node.right
+            if node.IsLeaf():
+                if node.value == 0xFF:
+                    break
+                decvals.append(node.value)
+                node = self.root
+            b = buffer.pop()
+        return decvals
+    
     def DrawTree(self, parent=None, graph=None):
         node = self.root if parent is None else parent
         graph = Graph() if graph is None else graph
@@ -124,8 +171,11 @@ if __name__ == "__main__":
     message = "ADA ATE AN APPLE"
     t = int()
     print(message)
-    h = Huffman(message)
+    h = Huffman()
+    h.FromString(message)
     print(h.codes)
     encoded_message = h.Encode(message)
     print(' '.join(format(b, '08b') for b in encoded_message))
     print(' '.join(format(b, '02X') for b in encoded_message))
+    decoded_message = h.Decode(encoded_message).decode()
+    print(decoded_message)
